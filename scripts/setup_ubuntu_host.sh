fc7d194a271732913e7d3df49b2b8e7cd8ff27a7
Billy Watson <billy@korrelate.com>
2014-11-15 12:16:09 -0500

    much simpler setup script that installs the newest docker

--- scripts/setup_ubuntu_host.sh
+++ scripts/setup_ubuntu_host.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 # this is a VERY basic setup script meant to run on ubuntu
+# this script must be run as root b/c of the fig install
 
-# must be run as root b/c of the fig install
-
-sudo apt-get -y update
-sudo apt-get -y install docker.io
-
-# docker tab completion (only lasts for this session)
-source /etc/bash_completion.d/docker.io
+curl -sSL https://get.docker.com/ubuntu/ | sudo sh
 
 curl -L https://github.com/docker/fig/releases/download/1.0.1/fig-`uname -s`-`uname -m` > /usr/local/bin/fig; chmod +x /usr/local/bin/fig
